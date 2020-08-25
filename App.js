d0caeabbcb5c7fff32815f28fc9a3c2b96afd9e8
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 17:50:49 -0600

    Using flexbox

--- App.js
+++ App.js
@@ -9,13 +9,42 @@ import {
 const App = () => {
   return (
     <>
-      
+      <View style={styles.container}>
+        <View style={styles.caja1}></View>
+        <View style={styles.caja2}></View>
+        <View style={styles.caja3}></View>
+        <View style={styles.caja4}></View>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  container:{
+    backgroundColor: 'cornflowerblue',
+    flex:1,
+    flexDirection: 'column',
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: 'navy',
+    flex: 1,
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: 'yellow',
+    flex: 1,
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: 'green',
+    flex: 1,
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: 'teal',
+    flex: 3,
+  },
 });
 
 export default App;
