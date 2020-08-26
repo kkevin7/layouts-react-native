613375b59459bcdc41a1623ad3527f8eb041c86a
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 19:31:26 -0600

    Style using flexbox

--- App.js
+++ App.js
@@ -69,6 +69,41 @@ const App = () => {
             </View>
           </View>
         </View>
+
+        <Text style={styles.titulo}>Hospedajes en Los Angeles</Text>
+        <View
+          style={styles.listado}
+        >
+            <View style={styles.listadoItem}>
+              <Image
+              style={styles.mejores}
+                source={require('./assets/img/hospedaje1.jpg')}
+              />
+              <Text>Casa 3 Recamaras</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+              style={styles.mejores}
+                source={require('./assets/img/hospedaje2.jpg')}
+              />
+              <Text>Casa 3 Recamaras</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+              style={styles.mejores}
+                source={require('./assets/img/hospedaje3.jpg')}
+              />
+              <Text>Casa 3 Recamaras</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+              style={styles.mejores}
+                source={require('./assets/img/hospedaje4.jpg')}
+              />
+              <Text>Casa 3 Recamaras</Text>
+            </View>
+        </View>
+
       </ScrollView>
     </>
   );
@@ -99,6 +134,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     marginVertical: 5,
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  listadoItem:{
+    flexBasis: '49%',
   }
 });
 
