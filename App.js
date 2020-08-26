b2f912a53a757c5d42361386c2393140b99bb829
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 18:57:48 -0600

    Using scrollview horizontal and vertical

--- App.js
+++ App.js
@@ -11,6 +11,42 @@ const App = () => {
             source={require('./assets/img/bg.jpg')}
           />
         </View>
+
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en Paris</Text>
+          <ScrollView horizontal>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad5.jpg')}
+              />
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </>
   );
@@ -19,11 +55,24 @@ const App = () => {
 const styles = StyleSheet.create({
   banner: {
     height: 250,
-    flex: 1
+    flex: 1,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
   },
   bannerContainer: {
-    flexDirection: 'row'
-  }
+    flexDirection: 'row',
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
 });
 
 export default App;
