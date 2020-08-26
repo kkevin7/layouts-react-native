e87c43cadf803700794db5259d73d48728d51fdd
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 18:36:14 -0600

    Import images and use a background

--- App.js
+++ App.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
+import {StyleSheet, ScrollView, View, Text, Image} from 'react-native';
 
 const App = () => {
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.caja1}></View>
-        <View style={styles.caja2}></View>
-        <View style={styles.caja3}></View>
-        <View style={styles.caja4}></View>
+      <View>
+        <View style={styles.bannerContainer}>
+          <Image
+            style={styles.banner}
+            source={require('./assets/img/bg.jpg')}
+          />
+        </View>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor: 'cornflowerblue',
-    flex:1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    // alignItems: 'center',
+  banner: {
+    height: 250,
+    flex: 1
   },
-  caja1: {
-    padding: 20,
-    backgroundColor: 'navy',
-  },
-  caja2: {
-    padding: 20,
-    backgroundColor: 'yellow',
-  },
-  caja3: {
-    padding: 20,
-    backgroundColor: 'green',
-  },
-  caja4: {
-    padding: 20,
-    backgroundColor: 'teal',
-  },
+  bannerContainer: {
+    flexDirection: 'row'
+  }
 });
 
 export default App;
